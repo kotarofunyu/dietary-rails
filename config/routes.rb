76dcd801f6f8f82168dcc02c394e32005d2f0468
kotarofunyu <kotarofunyu@gmail.com>
2021-05-30 16:55:06 +0900

    ホーム画面指定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :cards
   resources :diaries
   devise_for :users
+
+  root to: 'diaries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
