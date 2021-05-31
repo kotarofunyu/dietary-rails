d28abb83f6ce866ae608885a7c6ebf7da51e5d70
kotarofunyu <kotarofunyu@gmail.com>
2021-05-31 22:16:46 +0900

    feat: rails g migration & db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_040338) do
+ActiveRecord::Schema.define(version: 2021_05_31_131453) do
 
   create_table "cards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
