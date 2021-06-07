0266bef5d2c0e63fb1740ccf1a390778032db0a5
kotarofunyu <kotarofunyu@gmail.com>
2021-06-07 22:38:56 +0900

    closeアイコンクリックでfadeOutする

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,10 +1,19 @@
 // Loads all Semantic javascripts
 // = require semantic-ui
 document.addEventListener('DOMContentLoaded', () => {
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
   $('#js-sidebar').on('click', function() {
     $('.ui.sidebar').sidebar('toggle');
   })
 
   $('.ui.dropdown').dropdown();
+
 })
 
